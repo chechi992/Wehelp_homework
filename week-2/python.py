b20de9215acc8a52a226f031834d94f1c4663b4d
chechi992 <107464637+chechi992@users.noreply.github.com>
2022-09-27 22:29:59 +0800

    0927

---
+++ week-2/python.py
@@ -0,0 +1,12 @@
+def calculate(min, max, step):
+
+    a = min + max + step
+    return a
+
+calculate(1, 3, 1) # 你的程式要能夠計算 1+2+3，最後印出 6
+calculate(4, 8, 2) # 你的程式要能夠計算 4+6+8，最後印出 18
+calculate(-1, 2, 2) # 你的程式要能夠計算 -1+1，最後印出 0
+
+print(calculate(1, 3, 1))
+
+
