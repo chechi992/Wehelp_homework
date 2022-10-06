7cc6f2996657840b07a0bac4421b35c87c855a1a
chechi992 <107464637+chechi992@users.noreply.github.com>
2022-10-07 01:30:40 +0800

    1007

--- week-3/crawler.py
+++ week-3/crawler.py
@@ -6,7 +6,7 @@ import re
 #     "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36 Edg/105.0.1343.53"
 #     })
 
-for page in range(9494,9504):
+for page in range(9494,9599,1):
 
     url="https://www.ptt.cc/bbs/movie/index"+str(page)+".html"
     request=req.Request(url, headers={
@@ -19,8 +19,18 @@ for page in range(9494,9504):
     import bs4
     root = bs4.BeautifulSoup(data,"html.parser")
 
-    with open("movie.txt","w",encoding="utf-8") as file:
+    with open("movie.txt","a",encoding="utf-8") as file:
         titles = root.find_all("div",class_="title")
         for title in titles:
-            if title.a !=None and '好' in title.a.string  :
+            if title.a !=None and '好雷' in title.a.string  :
                 file.write(title.a.string+"\n")
+        for title_pu in titles:
+            if title_pu.a !=None and '普雷' in title_pu.a.string  :
+                file.write(title_pu.a.string+"\n")
+        for title_fu in titles:
+            if title_fu.a !=None and '負雷' in title_fu.a.string  :
+                file.write(title_fu.a.string+"\n")
+
+
+
+
--- week-3/data.py
+++ week-3/data.py
@@ -1,13 +1,15 @@
 #網路連線
 import urllib.request as request
 import json
+
 import re
+
 src="https://padax.github.io/taipei-day-trip-resources/taipei-attractions-assignment.json"
 with request.urlopen(src)as response: 
     data = json.load(response)
     clist=data["result"]["results"]
     with open("data.csv","w",encoding="utf-8") as file:
         for place in clist:
-            file.write(place["stitle"] + "," + place["address"][5:8] + "," + place["longitude"] + "," + place["latitude"] + "," + re.split('(jpg)',place["file"])+ "\n")
+            file.write(place["stitle"] + "," + place["address"][5:8] + "," + place["longitude"] + "," + place["latitude"] + "," + str(place["file"].split("(http)")[0::1])+ "\n")
 
 
