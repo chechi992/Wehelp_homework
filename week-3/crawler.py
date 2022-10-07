cdcdfd74c406ef9f7d4fb5de3d64d5d149c1f081
chechi992 <107464637+chechi992@users.noreply.github.com>
2022-10-08 03:32:15 +0800

    1008

--- week-3/crawler.py
+++ week-3/crawler.py
@@ -1,36 +1,53 @@
+import string
 import urllib.request as req
 import re
+import bs4
+
 
 # url = "https://www.ptt.cc/bbs/movie/index.html"
 # request=req.Request(url, headers={
 #     "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36 Edg/105.0.1343.53"
 #     })
 
-for page in range(9494,9599,1):
+for page in range(9494,9504,1):
 
     url="https://www.ptt.cc/bbs/movie/index"+str(page)+".html"
     request=req.Request(url, headers={
-     "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36 Edg/105.0.1343.53"
-     })
-    
+        "User-Agent":"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Mobile Safari/537.36 Edg/105.0.1343.53"
+        })
+        
     with req.urlopen(request) as response:
         data = response.read().decode("utf-8")
+        root = bs4.BeautifulSoup(data,"html.parser")
+        
+        good = []
+        simple = []
+        bad = []
+        title = root.select("div.title a") #取HTML標中的 <div class="title"></div> 中的<a>標籤存入sel 
 
-    import bs4
-    root = bs4.BeautifulSoup(data,"html.parser")
-
-    with open("movie.txt","a",encoding="utf-8") as file:
-        titles = root.find_all("div",class_="title")
-        for title in titles:
-            if title.a !=None and '好雷' in title.a.string  :
-                file.write(title.a.string+"\n")
-        for title_pu in titles:
-            if title_pu.a !=None and '普雷' in title_pu.a.string  :
-                file.write(title_pu.a.string+"\n")
-        for title_fu in titles:
-            if title_fu.a !=None and '負雷' in title_fu.a.string  :
-                file.write(title_fu.a.string+"\n")
-
-
+        for s in title:
+            if '好雷' in s.text :
+                good.append(s.text)
+                for i in good:
+                    with open("movie.txt","w",encoding="utf-8") as file:  
+                        file.write(i+'\n')
+                        print(i)
+                        break
+                break
+        for d in title:            
+            if '普雷' in d.text :
+                simple.append(d.text)
+                for y in simple:
+                    with open("movie.txt","w",encoding="utf-8") as file:  
+                        file.write(y+'\n')
+                break
+        for e in title:            
+            if '負雷' in e.text :
+                bad.append(e.text)
+                for z in bad:
+                    with open("movie.txt","w",encoding="utf-8") as file:  
+                        file.write(z+'\n')
+                        print(z)
+                break
 
 
