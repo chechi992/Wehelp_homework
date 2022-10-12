fd7913929293698886b728e77d1cfc3ca5496096
chechi992 <107464637+chechi992@users.noreply.github.com>
2022-10-12 17:01:37 +0800

    1012加註解

--- week-4/app.py
+++ week-4/app.py
@@ -7,32 +7,33 @@ from flask import (
     url_for,
 )
 
-class User:
+class User: #定義user
     def __init__(self,id,username,password) :
         self.id = id
         self.username = username
         self.password = password
 
-    def __repr__(self):
+    def __repr__(self): # f-string(格式化字串常數) 類似JS的${}
         return f'<User:{self.username}>'    
 
 users = []
-users.append(User(id=1,username = 'test',password='test'))
+users.append(User(id=1,username = 'test',password='test')) #設定登入帳號密碼
 
 app = Flask(__name__,template_folder='./templates',static_url_path='/static') #__name__代表目前執行的模組
 
-app.config['SECRET_KEY'] = 'Your Key'
+app.config['SECRET_KEY'] = 'Your Key' #session 需設定 secret-key
 
 @app.route("/",methods = ['POST', 'GET']) #函式的裝飾(Decorator):以函式為基礎，提供附加的功能
 
 def login():
 
     if request.method == 'POST':
-        session.pop('user',None)
+
+        session.pop('user',None) #把資料清空
 
         if request.form['username'] == "" or request.form['password'] == "" :
 
-            return redirect(url_for('fail', message='請輸入帳號、密碼'))
+            return redirect(url_for('fail', message='請輸入帳號、密碼')) #在網址列後加入字串
 
         if request.form['username'] == 'test' and request.form['password'] == 'test':
 
@@ -49,8 +50,7 @@ def login():
 @app.route("/fail") 
 def fail():
 
-    message = request.args['message']
-    print(message)
+    message = request.args['message'] # query-string
     return render_template('fail.html', message = message) #傳遞參數
 
 @app.route("/profile") #代表我們要處理的網站路徑
@@ -63,7 +63,7 @@ def profile():
 
 @app.route('/signout')# 登出頁面
 def signout():
-    session.pop('user',None)
+    session.pop('user',None) # 清空資料
     return redirect(url_for('login')) 
 
 
