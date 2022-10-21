4e2713c6c4594e2126374d2405e6b2d7bc186ea3
chechi992 <107464637+chechi992@users.noreply.github.com>
2022-10-21 19:46:58 +0800

    Update data.sql

--- week-5/data.sql
+++ week-5/data.sql
@@ -1,13 +1,13 @@
--- MariaDB dump 10.19  Distrib 10.4.21-MariaDB, for osx10.10 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.31, for macos12 (x86_64)
 --
--- Host: localhost    Database: website
+-- Host: 127.0.0.1    Database: website
 -- ------------------------------------------------------
--- Server version	10.4.21-MariaDB
+-- Server version	5.5.5-10.4.21-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `member`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `member` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT '獨立編號',
   `name` varchar(255) NOT NULL COMMENT '姓名',
@@ -39,17 +39,6 @@ CREATE TABLE `member` (
 
 LOCK TABLES `member` WRITE;
 /*!40000 ALTER TABLE `member` DISABLE KEYS */;
-INSERT INTO `member` VALUES (1,'test2','test','test',600,'2022-10-20 17:23:12'),(2,'lorin','rin','poi',780,'2022-10-20 17:26:47'),(3,'erin','poi89','ury5677',3400,'2022-10-20 17:27:48'),(4,'yotty','tory','roi',830,'2022-10-20 17:28:25'),(5,'sally','daddy','mpmmy33',260,'2022-10-20 17:28:56');
+INSERT INTO `member` VALUES (1,'kevin','test','test',600,'2022-10-20 17:23:12'),(2,'lorin','rin','poi',780,'2022-10-20 17:26:47'),(3,'erin','poi89','ury5677',3400,'2022-10-20 17:27:48'),(4,'yotty','tory','roi',830,'2022-10-20 17:28:25'),(5,'sally','daddy','mpmmy33',260,'2022-10-20 17:28:56');
 /*!40000 ALTER TABLE `member` ENABLE KEYS */;
 UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2022-10-20 17:32:12
