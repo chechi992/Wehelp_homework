5e815cabef75281add268c6edea3c8fceb662303
chechi992 <107464637+chechi992@users.noreply.github.com>
2022-10-28 00:37:24 +0800

    Update app.py

--- week-6/app.py
+++ week-6/app.py
@@ -9,6 +9,7 @@ from flask import (
 )
 
 from flask_mysqldb import MySQL
+import MySQLdb.cursors
 
 class User: #定義user
     def __init__(self,id,username,password) :
@@ -44,24 +45,22 @@ def home():
 
 def signin():
 
-    if request.method == 'POST':
-
-        session.pop('user',None) #把資料清空
-
-        if request.form['username'] == "" or request.form['password'] == "" :
-
-            return redirect(url_for('error', message='請輸入帳號、密碼')) #在網址列後加入字串
-
-        if request.form['username'] == 'test' and request.form['password'] == 'test':
-
-            session["user"] = request.form['username']
-
-            return redirect(url_for('member'))       
-
-        if request.form['username'] != 'test' or request.form['password'] != 'test':
-
-            return redirect(url_for('error', message='帳號、或密碼輸入錯誤'))
-    
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = % s AND password = % s', (username, password, ))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            msg = 'Logged in successfully !'
+            return render_template('index.html', msg = msg)
+        else:
+            msg = 'Incorrect username / password !'
+            
     return render_template('login.html')
 
 
